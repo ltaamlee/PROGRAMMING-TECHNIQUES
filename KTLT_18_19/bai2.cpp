14ae9a8dc208c5a1200658019a4107ff12f29d03
LTaam <167110901+ltaamlee@users.noreply.github.com>
2024-05-20 23:34:09 +0700

    bai2.cpp

---
+++ KTLT_18_19/bai2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Try(vector <int> &a, int n, int k, int cursum, int curid, int &msum){
+	if (curid==n){
+		if (cursum%k==0){
+			msum=max(msum,cursum);
+		}
+		return;
+	}
+	Try(a,n,k,cursum,curid+1,msum);
+	Try(a,n,k,cursum+a[curid],curid+1,msum);	
+}
+int res(vector <int> &a, int n, int k){
+	int ans=0;
+	Try(a,n,k,0,0,ans);
+	return ans;
+}
+int main(){
+    ios_base::sync_with_stdio(NULL); cin.tie(NULL); cout.tie(NULL);
+    int n,k; cin>>n>>k;
+    vector <int> a(n);
+    int sum=0;
+    for (auto &i:a){
+    	cin>>i;
+    	sum+=i;
+	}
+	cout<<res(a,n,k);
+    return 0;
+}
+/*
+5 7
+33 54 51 48 15 
+201
+*/
